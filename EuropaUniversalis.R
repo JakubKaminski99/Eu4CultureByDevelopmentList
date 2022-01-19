0958499617aad2e795d0913086a919b2ce979214
Jakub Kaminski <jakub.kaminski99@wp.pl>
2022-01-19 17:46:00 +0100

    Pierwsze dodanie

---
+++ EuropaUniversalis.R
@@ -0,0 +1,23 @@
+# install.packages("tidyverse")
+# install.packages("XML")
+# install.packages("rvest")
+
+library(tidyverse)
+library(XML)
+library(rvest)
+
+# link <- readline(prompt = "Enter page link: ")
+page <- read_html("https://eu4.paradoxwikis.com/Economic_list_of_provinces")
+tables <- page %>% html_table(fill=TRUE)
+tableE <- tables[[1]]
+page <- read_html("https://eu4.paradoxwikis.com/Political_list_of_provinces")
+tables <- page %>% html_table(fill=TRUE)
+tableP <- tables[[1]]
+
+ecopol <- merge(tableE, tableP, by=c("ID")) 
+ecopol <- ecopol[c(3,11,13)]
+
+culturePower <- aggregate(ecopol$Development, by=list(Category=ecopol$Culture), FUN=sum)
+culturePower <- culturePower[order(culturePower$x,decreasing = TRUE),]
+
+
